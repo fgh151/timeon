14524ddaaa38b3eefc6690fa5e0aa314e73c741e
Fedor B Gorsky <fedor@support-pc.org>
2022-07-03 06:33:24 +0300

    info page

--- src/VInfo.js
+++ src/VInfo.js
@@ -5,13 +5,15 @@ const VInfo = () => {
 
     let params = useParams();
 
+    const event = JSON.parse(atob(params.id));
+
     return (
         <div className='row'>
             <div className='col-12'>
                 <div className='mt-5'>
-                    <p>Event: {params.id}</p>
-                    <p>Description: 123</p>
-                    <p>Price: 100500 TON</p>
+                    <p>Event: {event.title}</p>
+                    <p>Description: {event.description}</p>
+                    <p>Price: {event.price} TON</p>
                     <Link className='btn btn-primary' to={'/ton/visitor/translation/' + params.id}>Start event</Link>
                 </div>
             </div>
